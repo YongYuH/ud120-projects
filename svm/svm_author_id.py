c8682a84ac004ae7c86dd0be20aba05ae14ebc81
YongYuH <larry830326@hotmail.com>
2017-01-30 15:59:24 +0800

    use support vector machine to identify author of text

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,28 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+# clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+count = 0
+for i in pred:
+    if i == 1:
+        count += 1
+print count
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "training time:", round(time()-t0, 3), "s"
+print accuracy
 
 #########################################################
-
-
