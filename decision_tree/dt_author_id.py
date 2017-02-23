fdf4a6c0e00215250137528bb0125bf46285800b
YongYuH <larry830326@hotmail.com>
2017-02-23 15:03:58 +0800

    finish decision tree practice

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,12 +19,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# print len(features_train[0])
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "training time:", round(time()-t0, 3), "s"
+print accuracy
 
 #########################################################
 
